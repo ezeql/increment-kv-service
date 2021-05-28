301411e0294f773ff42d6486979d673de47804f6
Ezequiel Moreno <ezeqle@gmail.com>
2021-05-28 15:21:46 -0300

    cosmetocs: fix typos

--- testdata/values_test.go
+++ testdata/values_test.go
@@ -11,7 +11,7 @@ func Test_fillMask(t *testing.T) {
 		args args
 		want string
 	}{
-		// we want to generate a uuid with all items being the arg, being 0 in this case
+		// we want to generate an uuid with all digits being the args passed being 0 in this case.
 		{name: "test1", args: args{0}, want: "00000000-0000-0000-0000-000000000000"},
 		// save as above as using 2
 		{name: "test2", args: args{2}, want: "22222222-2222-2222-2222-222222222222"},
